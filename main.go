46178d17b620b8c030db939a74fae9e951135959
Paul Ingles <paul@oobaloo.co.uk>
2014-12-12 19:27:40 +0000

    add notes for gossip delegate

--- main.go
+++ main.go
@@ -24,29 +24,41 @@ func NewGossipDelegate(g *Gossiper) *GossipDelegate {
 	return &GossipDelegate{gossiper: g}
 }
 
+// return a slice of byte arrays containing messages to broadcast
+
+// overhead is amount of overhead in packet, limit is total size. total
+// message size must be smaller than limit
 func (m *GossipDelegate) GetBroadcasts(overhead, limit int) [][]byte {
 	empty := make([][]byte, 0)
 	return empty
 }
 
+// notified when we receive a message. blocking is bad as it would block
+// the whole udp loop. also, byte slice may be modified so a copy should
+// be taken.
+func (m *GossipDelegate) NotifyMsg(msg []byte) {
+	log.Println("delegate: notify msg")
+}
+
+
+// send local state to remote side, bool indicates as a
+// result of a join
 func (m *GossipDelegate) LocalState(join bool) []byte {
-	log.Println("delegate: local state")
+	log.Println("delegate: local state, joining", join)
 	empty := make([]byte, 0)
 	return empty
 }
 
+// merge remote side state
 func (m *GossipDelegate) MergeRemoteState(s []byte, join bool) {
 	log.Println("delegate: merge remote state")
 }
 
+// provide meta-data used when broadcasting an alive message
 func (m *GossipDelegate) NodeMeta(limit int) []byte {
 	log.Println("delegate: node meta")
 	empty := make([]byte, 0)
 	return empty
-}
-
-func (m *GossipDelegate) NotifyMsg(msg []byte) {
-	log.Println("delegate: notify msg")
 }
 
 //////////////////
@@ -108,6 +120,27 @@ func AttachShutdownHandler(g *Gossiper) {
 	}()
 }
 
+type BindAddr struct {
+	Address string
+	Port    int
+}
+
+func AddressAndPort(s string) (*BindAddr, error) {
+	bindParts := strings.Split(s, ":")
+	b := &BindAddr{}
+	b.Address = bindParts[0]
+	
+	if len(bindParts) > 1 {
+		bindPort, err := strconv.Atoi(bindParts[1])
+		if err != nil {
+			return nil, err
+		}
+		b.Port = bindPort
+	}
+	
+	return b, nil
+}
+
 func main() {
 	hostname, _ := os.Hostname()
 	
@@ -116,15 +149,15 @@ func main() {
 	var name = flag.String("name", hostname, "node name")
 	flag.Parse()
 	
+	binding, err := AddressAndPort(*bindAddressAndPort)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
 	config := ml.DefaultLocalConfig()
-	bindParts := strings.Split(*bindAddressAndPort, ":")
-	config.BindAddr = bindParts[0]
-	if len(bindParts) > 1 {
-		bindPort, err := strconv.Atoi(bindParts[1])
-		if err != nil {
-			log.Fatal("invalid bind address", err)
-		}
-		config.BindPort = bindPort
+	config.BindAddr = binding.Address
+	if binding.Port > 0 {
+		config.BindPort = binding.Port
 	}
 	config.Name = *name
 	
